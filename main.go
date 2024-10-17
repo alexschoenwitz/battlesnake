9a6c66dd531fe7ad5b3be1aefef525b862fbcf46
Alexandre Schönwitz <95051128@mail.schwarz>
2024-10-17 18:03:10 +0200

    test

--- main.go
+++ main.go
@@ -53,11 +53,11 @@ func move(state GameState) BattlesnakeMoveResponse {
 	fmt.Printf("%+v\n", myHead)
 	if myNeck.X < myHead.X || myHead.X == 0 { // Neck is left of head, don't move left
 		isMoveSafe["left"] = false
-	} else if myNeck.X > myHead.X || myHead.X == boardWidth { // Neck is right of head, don't move right
+	} else if myNeck.X > myHead.X || myHead.X == boardWidth-1 { // Neck is right of head, don't move right
 		isMoveSafe["right"] = false
 	} else if myNeck.Y < myHead.Y || myHead.Y == 0 { // Neck is below head, don't move down
 		isMoveSafe["down"] = false
-	} else if myNeck.Y > myHead.Y || myHead.Y == boardHeight { // Neck is above head, don't move up
+	} else if myNeck.Y > myHead.Y || myHead.Y == boardHeight-1 { // Neck is above head, don't move up
 		isMoveSafe["up"] = false
 	}
 
